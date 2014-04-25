ab0a6f13bcf9437f8e76a125354a615d8e4e0373
J-M <jean-michel.perraud@csiro.au>
2014-04-25 17:21:59 +1000

    Revert "Stop tracking files moved or otherwise"
    This reverts commit 99e34ba0f6e295bef44e2b13c788743e81ab8726.

---
+++ CSIRO.Metaheuristics.Tests/TestCovarianceContravariance.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CSIRO.Metaheuristics.Tests
+{
+    [TestFixture]
+    public class TestCovarianceContravariance
+    {
+        public void TestObjectiveEvaluators( )
+        {
+            var general = new ObjEvalA( );
+            var scores = general.EvaluateScore( new SysConfigA() );
+            var sysConfig = scores.SystemConfiguration;
+            Assert.IsAssignableFrom( typeof( SysConfigA ), sysConfig );
+        }
+
+        private class SysConfigA : ISystemConfiguration
+        {
+
+            #region ISystemConfiguration Members
+
+            public string GetConfigurationDescription( )
+            {
+                throw new NotImplementedException( );
+            }
+
+            public void ApplyConfiguration( object system )
+            {
+                throw new NotImplementedException( );
+            }
+
+            #endregion
+        }
+
+        private class ObjEvalA : IObjectiveEvaluator<SysConfigA>
+        {
+            #region IObjectiveEvaluator<SysConfigA> Members
+
+            public IObjectiveScores<SysConfigA> EvaluateScore( SysConfigA systemConfiguration )
+            {
+                throw new NotImplementedException( );
+            }
+
+            #endregion
+        }
+    }
+}
