aa0078556335d9c3863200f6abe7df68a659816d
Jean-Michel Perraud <jean-michel.perraud@csiro.au>
2015-02-06 16:45:26 +1100

    Fix for https://github.com/jmp75/rClr/issues/5

--- CSIRO.Metaheuristics/Objectives/Evaluations.cs
+++ CSIRO.Metaheuristics/Objectives/Evaluations.cs
@@ -16,7 +16,12 @@ namespace CSIRO.Metaheuristics.Objectives
 
             var procCount = System.Environment.ProcessorCount;
             if (!evaluator.SupportsThreadSafeCloning)
+            {
+                // Changes needed to solve https://github.com/jmp75/rClr/issues/5
                 procCount = 1;
+            }
+            IObjectiveScores[] result = new IObjectiveScores[population.Length];
+
             T[][] subPop = MetaheuristicsHelper.MakeBins(population, procCount);
             var offsets = new int[subPop.Length];
             IClonableObjectiveEvaluator<T>[] cloneEval = new IClonableObjectiveEvaluator<T>[subPop.Length];
@@ -28,22 +33,28 @@ namespace CSIRO.Metaheuristics.Objectives
                 cloneEval[i] = evaluator.Clone();
             }
 
-            IObjectiveScores[] result = new IObjectiveScores[population.Length];
-            Parallel.For(0, subPop.Length, parallelOptions, i =>
+            if (evaluator.SupportsThreadSafeCloning)
             {
-                var offset = offsets[i];
-                for (int j = 0; j < subPop[i].Length; j++)
-                {
-                    if (!isCancelled())
-                        result[offset + j] = cloneEval[i].EvaluateScore(population[offset + j]);
-                    else
-                        result[offset + j] = null;
-                }
+                Parallel.For (0, subPop.Length, parallelOptions, i => {
+                    EvaluateScores (population, isCancelled, offsets, subPop, i, result, cloneEval [i]);
+                });
+                    
+            } else {
+                EvaluateScores (population, isCancelled, offsets, subPop, 0, result, cloneEval [0]);
             }
 
-            );
             return result;
         }
 
+        static void EvaluateScores<T>(T[] population, Func<bool> isCancelled, int[] offsets, T[][] subPop, int i, IObjectiveScores[] result, IClonableObjectiveEvaluator<T> evaluator) where T : ISystemConfiguration
+        {
+            var offset = offsets [i];
+            for (int j = 0; j < subPop [i].Length; j++) {
+                if (!isCancelled ())
+                    result [offset + j] = evaluator.EvaluateScore (population [offset + j]);
+                else
+                    result [offset + j] = null;
+            }
+        }
     }
 }
