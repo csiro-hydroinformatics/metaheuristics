c5d4c076adb566435e1594f48685ae3ae8300760
J-M <jean-michel.perraud@csiro.au>
2015-04-01 18:10:39 +1100

    Fix nuget dependencies discrepancies

--- CSIRO.Modelling.Core/Properties/AssemblyInfo.cs
+++ CSIRO.Modelling.Core/Properties/AssemblyInfo.cs
@@ -7,12 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("CSIRO.Modelling.Core")]
 [assembly: AssemblyDescription("A software pattern to organise and manipulate model simulations more consistently across projects")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CSIRO")]
 [assembly: AssemblyProduct("CSIRO.Modelling.Core")]
-[assembly: AssemblyCopyright("Copyright © CSIRO 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f7b05199-11e7-49c4-b32c-d960f469a6d1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.1.0")]
-[assembly: AssemblyFileVersion("0.5.1.0")]
