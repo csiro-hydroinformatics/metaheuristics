3a31ed4f01b55fe09aef0a84c659c08b4c59d720
J-M <jean-michel.perraud@csiro.au>
2015-08-11 11:29:43 +1000

    Minor upgrades for rerelease to update depedency on R.NET

--- R/pkgs/mh/R/mh-package.r
+++ R/pkgs/mh/R/mh-package.r
@@ -9,14 +9,15 @@
 #' \tabular{ll}{
 #' Package: \tab mh\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.4-8\cr
-#' Date: \tab 2015-06-23\cr
-#' Release Notes: \tab Fix to AsDataFrame, which was checking for HyperCube<double> instead of IHyperCube<double> \cr
+#' Version: \tab 0.4-9 \cr
+#' Date: \tab 2015-08-11 \cr
+#' Release Notes: \tab Upgrade dependency on R.NET 1.6.5 \cr
 #' License: \tab LGPL 3\cr
 #' }
 #'
 #' \tabular{lll}{
 #' Version \tab Date \tab Notes \cr
+#' 0.4-8: \tab 2015-06-23 \tab Fix to AsDataFrame, which was checking for HyperCube<double> instead of IHyperCube<double> \cr
 #' 0.4-7: \tab 2015-06-18 \tab Fix SCE option parameter controlling SCE behaviorwas not passed to the engine. Update to use latest R.NET and Metaheuristics nugets \cr
 #' 0.4-6: \tab 2015-05-07 \tab Fix issue building on Ubuntu and R 3.2 \cr
 #' 0.4-5: \tab 2015-04-04 \tab Update to use CSIRO.Metaheuristics 0.4.9 - use parallel.foreach to work around Mono 3.12.1 \cr
