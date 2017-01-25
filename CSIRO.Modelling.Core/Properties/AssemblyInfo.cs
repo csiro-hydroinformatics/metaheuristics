b798ead0dcc489b8abf6232e9ba094a094f672a9
Daniel Collins <jugglindan@gmail.com>
2017-01-25 13:44:06 +0800

    updating all projects to use the shared solution assembly properties file. This will fix the inconsistent assembly versions in recent nuget package builds

--- CSIRO.Modelling.Core/Properties/AssemblyInfo.cs
+++ CSIRO.Modelling.Core/Properties/AssemblyInfo.cs
@@ -16,21 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f7b05199-11e7-49c4-b32c-d960f469a6d1")]
-
-[assembly: AssemblyCompany( "CSIRO" )]
-[assembly: AssemblyCopyright( "Copyright © CSIRO 2010-2014" )]
-[assembly: AssemblyTrademark( "" )]
-[assembly: AssemblyCulture( "" )]
-[assembly: AssemblyConfiguration("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion( "0.7.9.0" )]
-[assembly: AssemblyFileVersion( "0.7.9.0" )]
